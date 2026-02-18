fa42b3f225445b13877aaa599f31ea1ebfafc249
Dante Grieco <dantegrieco11@gmail.com>
2026-02-18 15:11:32 -0800

    feat: add user table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
